3f0a7516539456be9cdc926d6dd1e4525e625bcf
lalmeras <laurent.almeras@openwide.fr>
2014-10-22 13:27:54 +0200

    Automatic switch between squid3 / squid Use readlink instead of realpath Automatic cache initialization on first run

--- squid/run_squid.sh
+++ squid/run_squid.sh
@@ -1,12 +1,24 @@
 #! /bin/bash
 
-SCRIPT="$(realpath -e $0)"
+SCRIPT="$(readlink -f $0)"
 HERE="$(dirname "$SCRIPT")"
 
 sed "s@__HERE__@$HERE@" "${HERE}/squid.conf.tpl" > "${HERE}/squid.conf"
 
+if which squid > /dev/null; then
+	SQUID=squid
+elif which squid3 > /dev/null; then
+	SQUID=squid3
+fi
+
+if [ ! -f "${HERE}/cache/swap.state" ]; then
+	echo "Initializing cache..."
+	$SQUID -f "${HERE}/squid.conf" -z -N
+	echo "Cache initialized."
+fi
+
 if which multitail > /dev/null 2>&1; then
-	squid -f "${HERE}/squid.conf" -N 2>&1 | multitail -j --retry-all -i "${HERE}/log/cache.log" "${HERE}/log/access.log"
+	$SQUID -f "${HERE}/squid.conf" -N 2>&1 | multitail -j --retry-all -i "${HERE}/log/cache.log" "${HERE}/log/access.log"
 else
-	squid -f "${HERE}/squid.conf" -N
+	$SQUID -f "${HERE}/squid.conf" -N
 fi
