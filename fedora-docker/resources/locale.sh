8a1d02c23a54a86533018b95bca15b4962c8605a
Laurent Almeras <lalmeras@gmail.com>
2014-12-18 22:45:23 +0100

    localedef --list-archive | grep needs -a as output may be binary

--- fedora-docker/resources/locale.sh
+++ fedora-docker/resources/locale.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
-localedef --list-archive | grep -v -i ^en | xargs localedef --delete-from-archive
+# -a is needed as list-archive output may be binary
+localedef --list-archive | grep -a -v -i ^en | xargs localedef --delete-from-archive
 mv /usr/lib/locale/locale-archive /usr/lib/locale/locale-archive.tmpl
 build-locale-archive
