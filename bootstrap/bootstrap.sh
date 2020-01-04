6bce924663315a7ef2973d384bb6ac263024a5d4
Laurent Almeras <lalmeras@gmail.com>
2020-01-04 13:10:59 +0100

    update poetry

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -3,8 +3,8 @@
 THIS_SCRIPT="$( realpath "$0" )"
 THIS_PATH="$( dirname "$THIS_SCRIPT" )"
 : ${BOOTSTRAP_PY_BRANCH:=master}
-: ${BOOTSTRAP_PY:=https://raw.githubusercontent.com/lalmeras/clickable_bootstrap/${BOOTSTRAP_PY_BRANCH}/bootstrap.py}
+: ${BOOTSTRAP_PY:=https://raw.githubusercontent.com/lalmeras/clickable_bootstrap/master/bootstrap.py}
 
-BOOTSTRAP_COMMAND="poetry config settings.virtualenvs.create false && cd '$THIS_PATH' && poetry install -v"
+BOOTSTRAP_COMMAND="poetry config virtualenvs.create false && cd '$THIS_PATH' && poetry install -v"
 
-curl -s -o - "${BOOTSTRAP_PY}" | BOOTSTRAP_COMMAND="$BOOTSTRAP_COMMAND" BOOTSTRAP_PATH="$THIS_PATH" python - "$@"
+curl -sLJ "${BOOTSTRAP_PY}" | BOOTSTRAP_COMMAND="$BOOTSTRAP_COMMAND" BOOTSTRAP_PATH="$THIS_PATH" python - "$@"
